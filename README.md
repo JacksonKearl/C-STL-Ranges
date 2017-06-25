3f8fa4079ea2a50da4396310557bef52bcb58633
Jackson Kearl <jkearl11@gmail.com>
2017-06-25 11:32:20 -0400

    Added additional extra files

---
+++ README.md
@@ -0,0 +1,2 @@
+# C++ STL Ranges
+Static lib for fun stream-like operations in C++
---
+++ Tests/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Tests
+//
+//  Created by Jackson Kearl on 6/25/17.
+//  Copyright © 2017 JK:GY. All rights reserved.
+//
+
+#include "Range.hpp"
+
+int main() {
+    Range r{};
+    r.HelloWorld("Hello");
+}
